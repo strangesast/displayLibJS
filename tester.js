f0f49565f55a380067da023580dae60689265f56
jimzag <jimzag@bellsouth.net>
2014-12-29 00:24:29 -0500

    compatibility with latest displayLib
    additional tweaks to the tester.js so it will show controls and text
    similar to msgTest

--- tester.js
+++ tester.js
@@ -8,24 +8,25 @@ var rect = dl.DLRect();
 rect.xy = new dl.XYInfo(0, 0, 120, 32);
 rect.line_color = new dl.DLColor(239,112,35);
 rect.panel = 1;
-rect.line_width = 1;
+rect.line_width = 2;
 
 var textbox = dl.DLTextbox();
 textbox.xy = new dl.XYInfo(3,3,110,10);
 textbox.fg_color = new dl.DLColor(43,89,249);
-textbox.bg_color = new dl.DLColor(249,197,166);
+//textbox.bg_color = new dl.DLColor(249,197,166);
+textbox.bg_color = new dl.DLColor(0,0,0);
 textbox.border_color = new dl.DLColor(0, 200, 0, 80);
-textbox.bg_color.set_intensity (80);
+textbox.border_color.set_intensity (80);
 textbox.border_width = 1;
-textbox.control = 12;                          
+textbox.control = 12;
 
 var text = dl.DLText();
-text.text = "ab";
+text.text = "A QUICK BROWN FOX";
 //text.text_action = TextAction.TEXT_APPEND;
-text.text_action = 1; //TEXT_APPEND
+text.text_action = 2; //TEXT_REPLACE
 text.message = 0;
 text.position = 0;
-text.parent_control = 12;                            
+text.parent_control = 12;
 
 
 
@@ -43,19 +44,19 @@ socket.connect (PORT, HOST, function() {
     var result = panel.BuildMessage ();
     var send_buf = result.result_buffer.slice(0,result.result_bytes);
     socket.write(send_buf);
-    
+
     result = rect.BuildMessage ();
     send_buf = result.result_buffer.slice(0,result.result_bytes);
     socket.write(send_buf);
-    
+
     result = textbox.BuildMessage ();
     send_buf = result.result_buffer.slice(0,result.result_bytes);
     socket.write(send_buf);
-    
+
     result = text.BuildMessage ();
     send_buf = result.result_buffer.slice(0,result.result_bytes);
     socket.write(send_buf);
-    
+
     console.log ("written");
     socket.destroy();
 })
